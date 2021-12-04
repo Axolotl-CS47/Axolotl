86627aaa69774d16d9a4ba1661890f88cd51caac
Genevieve Annable <genevieveannable@gmail.com>
2021-12-03 19:31:48 -0800

    map rendered

--- client/index.js
+++ client/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MapChart from './App.js'
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <MapChart />,
   document.getElementById('root')
 );
