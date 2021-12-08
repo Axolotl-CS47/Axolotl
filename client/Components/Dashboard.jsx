4b1ebc64efb870275be9fa2239b9badcddebbb78
Jay Wall <jaywall@gmail.com>
2021-12-07 19:39:07 -0800

    working map with tooltip

---
+++ client/Components/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+
+const Dashboard = () => {
+  
+  return (
+    <>
+        <div>
+        <h3>Dashboard</h3>
+        <hr></hr>
+
+        </div>
+    </>
+  );
+};
+
+export default Dashboard;
