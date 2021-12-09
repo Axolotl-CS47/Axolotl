11d799c9669631c7b481d65be5155667ca9f1ac4
Jay Wall <jaywall@gmail.com>
2021-12-08 22:14:46 -0800

    minor

--- client/utility.js
+++ client/utility.js
@@ -1,11 +1,5 @@
 export const findCityByCountry = (arr, countryName) => {
-<<<<<<< HEAD
-    const city = arr.filter((element) => element.country === countryName);
-    return city[0]?.city;
-  };
-=======
   const city = arr.filter((element) => element.country === countryName);
   return city[0]?.city;
 };
 
->>>>>>> d9475f875d1b6f333051bcd32ae17516f4cccc3e
