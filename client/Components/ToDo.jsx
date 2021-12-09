c70f8174b4c5bc23e5b68b09d6d50d918e2afedf
stephenkim612 <92137370+stephenkim612@users.noreply.github.com>
2021-12-09 13:20:06 -0800

    todo update

--- client/Components/ToDo.jsx
+++ client/Components/ToDo.jsx
@@ -6,25 +6,44 @@ const ToDo = () => {
   const [result, setResult] = useState('');
   const [countryCode, setCountryCode] = useState('');
   const [cities, setCities] = useState([]);
-
+  const [citySelected, setCitySelected] = useState('')
+  const [toDo, setToDo] = useState('');
+  const [newArray, setNewArray] = useState([]);
   const allCountriesObj = Country.getAllCountries();
-  const allCountries = allCountriesObj.map(obj =>
+  const allCountries = allCountriesObj?.map(obj =>
     <option>{obj.name}</option>
   )
 
 
-
-
-
-
-  const allTheCities = cities.map(element => {
+  const allTheCities = cities?.map(element => {
     return <option>{element}</option>
   })
 
+  const arrToDo = []
+  useEffect(() => {
 
-
-
-
+    fetch(`https://api.opentripmap.com/0.1/en/places/geoname?name=${citySelected}&apikey=5ae2e3f221c38a28845f05b63e95fc09f0bc72578a5aa05f060e818b`)
+    .then((res) => res.json())
+    .then(data => {
+        const lon = data.lon;
+        const lat = data.lat; 
+        return fetch(`https://api.opentripmap.com/0.1/en/places/radius?radius=1500&lon=${lon}&lat=${lat}&apikey=5ae2e3f221c38a28845f05b63e95fc09f0bc72578a5aa05f060e818b`)
+    })
+    .then(res => res.json())
+    .then(data => {
+      const arr = data.features?.map(element => element.properties.xid);
+      const arrayTest = []
+      for (let i = 0; i<5; i++) {
+        fetch(`https://api.opentripmap.com/0.1/en/places/xid/${arr[i]}?apikey=5ae2e3f221c38a28845f05b63e95fc09f0bc72578a5aa05f060e818b`)
+        .then(res => res.json())
+        .then(dataXXID => {
+         arrayTest.push(dataXXID)
+         console.log('dataxxid', arrayTest)
+        })
+      }
+    })
+    .catch(err => console.log(err));      
+  }, [citySelected])
 
 
   const handleClick = (value) => {
@@ -51,7 +70,7 @@ const ToDo = () => {
       <select onChange={(e) => handleClick(e.target.value)}>
         {allCountries}
       </select>
-      <select>
+      <select onChange={(e) => setCitySelected(e.target.value)}>
         <option>City</option>
         {allTheCities}
       </select>
